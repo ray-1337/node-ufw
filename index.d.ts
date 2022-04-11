d8b6fd6d8985bc806f98157b1b7a1e7c71d6a043
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-04-11 23:03:32 +0200

    added typing

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+declare const nodeUfw: {
+  name: string;
+
+  version: string;
+
+  disable: Promise<Boolean>;
+
+  enable: Promise<Boolean>;
+
+  status: (raw?: boolean) => Promise<string | Array<ParsedStatus>>;
+
+  allow: {
+    port: (port: number, protocol?: "udp" | "tcp") => Promise<Boolean>;
+    address: (address: string, port?: number, protocol?: "udp" | "tcp") => Promise<Boolean>;
+  };
+
+  deny: {
+    port: (port: number, protocol?: "udp" | "tcp") => Promise<Boolean>;
+    address: (address: string, port?: number, protocol?: "udp" | "tcp") => Promise<Boolean>;
+  };
+};
+
+interface ParsedStatus {
+  to: string;
+  action: string;
+  from: string;
+};
+
+export default nodeUfw;
